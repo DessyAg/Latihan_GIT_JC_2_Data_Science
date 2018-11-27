c9251d33e9feb8299d190677fd9a4ab708922802
Dessy <dessy.augustina@gmail.com>
2018-11-27 13:44:46 +0700

    commit 1

---
+++ latihan1.py
@@ -0,0 +1,31 @@
+# # bertasbih ='saya sedang bertasbih'
+# # bertasbih=True
+# # kucing = 50
+# # keren=20.5
+
+# # nama=input("what is your name : ")
+# # print(nama)
+
+# # nama= input("nama anda : ")
+# # umur=input("umur anda :")
+# # kelamin=input("JenKel :")
+# # pekerjaan=input("pekerjaan anda :")
+
+# # print("nama anda: " + nama)
+# # print("umur anda : " + umur)
+# # print("Jenis Kelamin : " + kelamin)
+# # print("Pekerjaan : " + pekerjaan)
+
+# angka1=input("masukkan angka pertama")
+# angka1=input("masukkan angka kedua")
+
+
+# # print(type(bertasbih))
+# # print(type(kucing))
+# # print(type(keren))
+
+
+# import math;
+
+# print(math.pi)
+# print(math.fabs(-4.7))
